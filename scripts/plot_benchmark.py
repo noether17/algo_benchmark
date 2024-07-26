251b37d6d430368631bf988fec9b21d411dbad0d
Jonathan White <jawh@umich.edu>
2024-07-26 12:47:49 -0600

    Modified benchmark and plotting script to run test with multiple element sizes.

--- scripts/plot_benchmark.py
+++ scripts/plot_benchmark.py
@@ -8,24 +8,34 @@ def main():
         data = json.load(input_file)
         caches = data["context"]["caches"]
         benchmarks = data["benchmarks"]
+
+        # parse data
         family_indices = np.array([int(bm["family_index"]) for bm in benchmarks])
-        unique_family_indices = np.unique(family_indices)
-        algo_names = np.array([str(bm["name"].split('<')[1].split('>')[0][:-2])
+        element_sizes = np.array([str(bm["name"].split(' ')[1].split('E')[0])
+                                  for bm in benchmarks])
+        algo_names = np.array([str(bm["name"].split('<')[1].split(',')[0][:-2])
                                for bm in benchmarks])
         array_sizes = np.array([int(bm["name"].split('/')[-1]) for bm in benchmarks])
         items_per_second = np.array([float(bm["items_per_second"]) for bm in benchmarks])
         seconds_per_item = 1.0 / items_per_second
 
-        for family in unique_family_indices:
-            current_indices = np.where(family_indices == family)
-            current_algo_name = algo_names[current_indices][0]
-            plt.loglog(array_sizes[current_indices], seconds_per_item[current_indices],
-                       label=current_algo_name)
-        plt.xlabel("Items in Array")
-        plt.ylabel("Time per Item (s)")
-        plt.legend()
-        plt.grid()
-        plt.show()
+        unique_element_sizes = ['Small', 'Medium', 'Large']
+        element_bytes = { 'Small': 4, 'Medium': 64, 'Large': 1024 }
+        for element_size in unique_element_sizes:
+            element_size_indices = np.where(element_sizes == element_size)
+            current_family_indices = family_indices[np.where(element_sizes == element_size)]
+            unique_family_indices = np.unique(current_family_indices)
+            for family_index in unique_family_indices:
+                current_indices = np.where(family_indices == family_index)
+                algo_name = algo_names[current_indices[0][0]]
+                plt.loglog(array_sizes[current_indices], seconds_per_item[current_indices], label=algo_name)
+            plt.xlabel("Items in Array")
+            plt.xlim(1, 1024)
+            plt.ylabel("Time per Item (s)")
+            plt.title(f"Performance of Sorting Algorithms with {element_bytes[element_size]}B Elements")
+            plt.legend()
+            plt.grid()
+            plt.show()
 
 if __name__ == "__main__":
     main()
--- src/benchmark/sorting_algos_benchmark.cpp
+++ src/benchmark/sorting_algos_benchmark.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 
 #include <algorithm>
+#include <array>
 #include <random>
 #include <vector>
 
@@ -79,20 +80,30 @@ struct StdSorter {
   }
 };
 
-template <typename SortType>
+template <int N>
+struct Element {
+  std::array<int, N> data{};
+  Element() = default;
+  Element(int i) : data{i} {}
+  friend bool operator<(Element const& a, Element const& b) {
+    return a.data[0] < b.data[0];
+  }
+};
+
+template <typename SortType, typename T>
 static void BM_Sort(benchmark::State& state) {
   auto const size = state.range(0);
 
   auto const total_items = size * n_repetitions;
   auto const reference_vector = [total_items]() {
-    auto v = std::vector<int>(total_items);
+    auto v = std::vector<T>(total_items);
     std::iota(v.begin(), v.end(), 0);
     auto gen = std::mt19937(0);
     std::shuffle(v.begin(), v.end(), gen);
     return v;
   }();
 
-  auto vector_copy = std::vector<int>(total_items);
+  auto vector_copy = std::vector<T>(total_items);
   for (auto _ : state) {
     std::copy(reference_vector.begin(), reference_vector.end(),
               vector_copy.begin());
@@ -111,30 +122,93 @@ static void BM_Sort(benchmark::State& state) {
   state.SetItemsProcessed(state.iterations() * total_items);
 }
 
-BENCHMARK_TEMPLATE(BM_Sort, NoSorter)
+using SmallElement = Element<1>;
+using MediumElement = Element<16>;
+using LargeElement = Element<256>;
+
+// Perform benchmarks with SmallElement
+BENCHMARK_TEMPLATE(BM_Sort, NoSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, InsertionSorter)
+BENCHMARK_TEMPLATE(BM_Sort, InsertionSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, BinaryInsertionSorter)
+BENCHMARK_TEMPLATE(BM_Sort, BinaryInsertionSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, BubbleSorter)
+BENCHMARK_TEMPLATE(BM_Sort, BubbleSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, QuickSorter)
+BENCHMARK_TEMPLATE(BM_Sort, QuickSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, HoareQuickSorter)
+BENCHMARK_TEMPLATE(BM_Sort, HoareQuickSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, HeapSorter)
+BENCHMARK_TEMPLATE(BM_Sort, HeapSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, MergeSorter)
+BENCHMARK_TEMPLATE(BM_Sort, MergeSorter, SmallElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
-BENCHMARK_TEMPLATE(BM_Sort, StdSorter)
+BENCHMARK_TEMPLATE(BM_Sort, StdSorter, SmallElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+
+// Repeat with MediumElement
+BENCHMARK_TEMPLATE(BM_Sort, NoSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, InsertionSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, BinaryInsertionSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, BubbleSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, QuickSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, HoareQuickSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, HeapSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, MergeSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, StdSorter, MediumElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+
+// Repeat with LargeElement
+BENCHMARK_TEMPLATE(BM_Sort, NoSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, InsertionSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, BinaryInsertionSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, BubbleSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, QuickSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, HoareQuickSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, HeapSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, MergeSorter, LargeElement)
+    ->RangeMultiplier(2)
+    ->Range(min_array_size, max_array_size);
+BENCHMARK_TEMPLATE(BM_Sort, StdSorter, LargeElement)
     ->RangeMultiplier(2)
     ->Range(min_array_size, max_array_size);
